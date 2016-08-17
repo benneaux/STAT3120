837a7ea25253d3512485d5a7b1c41de2f78b18f3
benneaux <benjamin.g.moran@gmail.com>
2016-08-17 16:40:49 +1000

    Initial Commit

---
+++ question6.R
@@ -0,0 +1,16 @@
+
+a <- 100
+x <- seq(a/1000,a, by=a/1000)
+
+samples <- c()
+for (i in 1:1000){
+  samples[i] <- max(sample(runif(10,0,a)))
+}
+dens <- density(samples)
+par(mfrow=c(2,1))
+plot(x,dunif(x,0,a))
+plot(dens)
+dev.off()
+plot(dens)
+lines(dunif(x,0,a))
+max(samples)
