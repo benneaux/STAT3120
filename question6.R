2fc1fc6cf1be43a3c2b5c6f2534ba6985998abc1
benneaux <benjamin.g.moran@gmail.com>
2016-08-19 13:13:56 +1000

    merging

--- question6.R
+++ question6.R
@@ -1,10 +1,10 @@
 
-a <- 100
+a <- 5
 x <- seq(a/1000,a, by=a/1000)
 
 samples <- c()
 for (i in 1:1000){
-  samples[i] <- max(sample(runif(10,0,a)))
+  samples[i] <- max(sample(runif(20,0,a)))
 }
 dens <- density(samples)
 par(mfrow=c(2,1))
