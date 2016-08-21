6a27d8ebfb286aec5e0450339b2af852f30dfd94
benneaux <benjamin.g.moran@gmail.com>
2016-08-21 12:49:19 +1000

    new answers to question 6. Also, got rid of unnescessary code for question 6 code example.

--- question6.R
+++ question6.R
@@ -1,20 +1,6 @@
 y <- 5
 n <- 20
 a <- 5
-x <- seq(a/1000,a, by=a/1000)
-
-samples <- c()
-for (i in 1:1000){
-  samples[i] <- max(sample(runif(20,0,a)))
-}
-dens <- density(samples)
-par(mfrow=c(2,1))
-plot(x,dunif(x,0,a))
-plot(dens)
-dev.off()
-plot(dens)
-lines(dunif(x,0,a))
-max(samples)
 
 theta <- seq(y+0.001,12, by = 0.001)
 post.dens <- 1/(theta^(n))
